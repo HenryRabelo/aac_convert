ddf49e446d4de5710c81de0746493c262179a10f
HenryRabelo <53522727+HenryRabelo@users.noreply.github.com>
2026-05-14 23:12:07 -0300

    Add files via upload

--- aac-convert.sh
+++ aac-convert.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set IFS (Internal Field Separator) exclusively to newlines during "for" loops
+IFS=$'\n'
+
 Help() {
 echo "
 Advanced Audio Codec Batch Converter script.
@@ -22,7 +25,8 @@ Commands:
 }
 
 ResolveCommands() {
-  if [ -z $(ffmpeg -loglevel 'quiet' -codecs | grep --only-matching 'libfdk_aac' | head -1) ]; then
+  local -r FDK=$(ffmpeg -loglevel 'quiet' -codecs | grep --only-matching 'libfdk_aac' | head -1)
+  if [ -z "$FDK" ]; then
     echo 'Error: Your build of FFMPEG has not enabled the libfdk_aac codec.'
     exit 1
   elif [ "$1" = '--help' ]; then
@@ -32,6 +36,10 @@ ResolveCommands() {
     if [ ! -f "$2" ]; then
       echo 'Error: File not found in this directory.'
       echo 'Please pass the file as an argument.'
+      exit 1
+    elif [ ! -z "$3" ]; then
+      echo 'Error: Invalid syntax.'
+      echo 'Please verify your command or consult --help.'
       exit 1
     fi
     Convert "$2"
@@ -50,6 +58,10 @@ ResolveCommands() {
       echo 'Error: Output directory not found.'
       echo 'Please create or pass a valid output path as an argument.'
       exit 1
+    elif [ ! -z "$4" ]; then
+      echo 'Error: Invalid syntax.'
+      echo 'Please verify your command or consult --help.'
+      exit 1
     fi
     Batch "$2" "$3"
     exit 0
@@ -60,47 +72,41 @@ ResolveCommands() {
 }
 
 BuildTags() {
-  TAG=''
+  local TAG=''
+  local TAGNAME=''
+  local VALUE=''
   local CUSTOM=''
   local DOMAIN='domain="com.apple.iTunes"'
   local TOTALTRACKS=$(echo "$1" | grep 'totaltracks' | cut -d'=' -f2-)
   local TOTALDISCS=$(echo "$1" | grep 'totaldiscs' | cut -d'=' -f2-)
   
-  IFS=$'\n'
   for DATA in $1; do
-    local TAGNAME=$(echo "$DATA" | cut -d'=' -f1)
-    local VALUE=$(echo "$DATA" | cut -d'=' -f2-)
+    TAGNAME=$(echo "$DATA" | cut -d'=' -f1)
+    VALUE=$(echo "$DATA" | cut -d'=' -f2-)
     
     # Skip if data somehow has no assigned value
     if [ -z "$VALUE" ]; then
       continue
-    elif [ "$TAGNAME" = 'musicbrainz_albumid' ]; then
-      CUSTOM='true'
-    elif [ "$TAGNAME" = 'releasecountry' ]; then
-      CUSTOM='true'
-    elif [ "$TAGNAME" = 'releasetype' ]; then
-      CUSTOM='true'
-    elif [ "$TAGNAME" = 'label' ]; then
-      CUSTOM='true'
-    elif [ "$TAGNAME" = 'replaygain_track_gain' ]; then
-      CUSTOM='true'
-    elif [ "$TAGNAME" = 'replaygain_album_gain' ]; then
-      CUSTOM='true'
-    elif [ "$TAGNAME" = 'replaygain_track_peak' ]; then
-      CUSTOM='true'
-    elif [ "$TAGNAME" = 'replaygain_album_peak' ]; then
+    elif [ "$TAGNAME" = 'musicbrainz_albumid' ] || \
+         [ "$TAGNAME" = 'releasecountry' ] || \
+         [ "$TAGNAME" = 'releasetype' ] || \
+         [ "$TAGNAME" = 'label' ] || \
+         [ "$TAGNAME" = 'replaygain_track_gain' ] || \
+         [ "$TAGNAME" = 'replaygain_album_gain' ] || \
+         [ "$TAGNAME" = 'replaygain_track_peak' ] || \
+         [ "$TAGNAME" = 'replaygain_album_peak' ]; then
       CUSTOM='true'
     elif [ "$TAGNAME" = 'bpm' ]; then
       CUSTOM='false'
     elif [ "$TAGNAME" = 'track' ]; then
       CUSTOM='false'
-      if [ ! -z "$TOTALTRACKS" ]; then
+      if [ "$TOTALTRACKS" -gt 0 ]; then
         VALUE="$VALUE/$TOTALTRACKS"
       fi
     elif [ "$TAGNAME" = 'disc' ]; then
       TAGNAME='disk'
       CUSTOM='false'
-      if [ ! -z "$TOTALDISCS" ]; then
+      if [ "$TOTALDISCS" -gt 0 ]; then
         VALUE="$VALUE/$TOTALDISCS"
       fi
     else
@@ -115,21 +121,23 @@ BuildTags() {
       TAG=$(echo "$TAG" '--rDNSatom' "\"$VALUE\"" "name=\"$TAGNAME\"" "$DOMAIN")
     fi
   done
-  unset IFS
-  unset CUSTOM
   
-  # Print finished tag as a means of returning the value
+  # Echo finished tag as a means of returning the value
   echo "$TAG"
 }
 
 Batch() {
+  local -r TOTALFILES=$(find "$1" -type f | grep --invert-match '.jpg\|.png\|.txt\|.lrc\|.m3u' | wc -l)
+  local -r INPUT=$(find "$1" -type d)
+  local -r OUTPUT="${2%/}"
   local INDICATOR=1
-  local TOTALFILES=$(find "$1" -type f | grep --invert-match '.jpg\|.png\|.txt\|.lrc\|.m3u' | wc -l)
-  local INPUT=$(find "$1" -type d)
-  local OUTPUT="$2"
+  local PREVIOUS=''
+  local FILES=''
+  local TYPE=''
+  local PERCENTAGE=''
+  local FILESREM=''
+  local TIMEREM=''
 
-  # Set IFS (Internal Field Separator) exclusively to newlines during "for" loop
-  IFS=$'\n'
   for DIRECTORY in $INPUT; do
     mkdir -p "$OUTPUT/$DIRECTORY"
     FILES=$(find "$DIRECTORY" -maxdepth 1 -type f)
@@ -139,9 +147,6 @@ Batch() {
       continue
     fi
     
-    echo 'Entering' $(basename "$DIRECTORY")
-    echo ''
-    
     for MUSIC in $FILES; do
       TYPE=$(file "$MUSIC" | grep --only-matching "image\|text" | head -1)
       
@@ -150,19 +155,29 @@ Batch() {
         continue
       fi
       
-      local PERCENTAGE=$(echo "scale=2; ($INDICATOR * 100 / $TOTALFILES)" | bc)
-      local FILESREM=$(( TOTALFILES - INDICATOR ))
-      local TIMEREM=$(( (($SECONDS * $TOTALFILES) / $INDICATOR) - $SECONDS ))
-      echo "$FILESREM files remaining - $PERCENTAGE% complete" '|' 'Runtime:' $(( SECONDS / 60 ))'m '$(( SECONDS % 60 ))'s' '-' $(( $TIMEREM / 60 ))'m '$(( $TIMEREM % 60 ))'s' 'remaining.'
-      Convert "$MUSIC" "$OUTPUT/"
+      clear
+      
+      if [ ! "$PREVIOUS" = '' ]; then
+        echo $(basename "$PREVIOUS") 'is done.'
+        echo ''
+      fi
+      
+      echo 'Converting:' $(basename "$DIRECTORY")
+      echo ''
+      
+      PERCENTAGE=$(echo "scale=2; ($INDICATOR * 100 / $TOTALFILES)" | bc)
+      FILESREM=$(( TOTALFILES - INDICATOR ))
+      TIMEREM=$(( (($SECONDS * $TOTALFILES) / $INDICATOR) - $SECONDS ))
+      
+      echo "$FILESREM files remaining - $PERCENTAGE% complete" '|' 'Remaining:' $(( $TIMEREM / 60 ))'m '$(( $TIMEREM % 60 ))'s' '-' 'Runtime:' $(( SECONDS / 60 ))'m '$(( SECONDS % 60 ))'s'
+      Convert "$MUSIC" "$OUTPUT"
+      
       ((INDICATOR++))
     done
     
-    echo $(basename "$DIRECTORY") 'is done.'
-    echo ''
+    PREVIOUS="$DIRECTORY"
+    
   done
-  unset IFS
-  # Reset (unset) IFS
   
   echo 'Batch conversion is finished.'
   echo ''
@@ -172,17 +187,19 @@ Batch() {
 Convert() {
   # Get relative path and filename without extension and problem characters
   local FILENAME=$(echo $(dirname "$1")'/'$(basename "$1" | sed -e 's/\.[^./]*$//' | tr -d "<>*|\\:/\"?"))
-  local CONVERT="${2}$FILENAME"
+  local CONVERT="${2}/$FILENAME"
   local METADATA=$(ffmpeg -loglevel 'quiet' -i "$1" -metadata 'LYRICS=' -f ffmetadata - | awk -F'=' 'BEGIN {OFS="="} NR > 1 { $1=tolower($1); print $0 }')
   local TAGS=$(BuildTags "$METADATA")
   
   echo 'Converting:' $(basename "$1")
-  ffmpeg -loglevel 'error' -stats -y -i "$1" -c:a libfdk_aac -afterburner 1 -cutoff 20000 -ar 44100 -vbr 5 -c:v png -vf scale=600:600:force_original_aspect_ratio=decrease:force_divisible_by=2 "$CONVERT.m4a"
   
+  ffmpeg -loglevel 'error' -stats -y -i "$1" -c:a libfdk_aac -afterburner 1 -cutoff 20000 -ar 44100 -vbr 5 -c:v png -vf scale=600:600:force_original_aspect_ratio=decrease:force_divisible_by=2 "$CONVERT.m4a"
   eval AtomicParsley "\"$CONVERT.m4a\"" --overWrite "$TAGS" >/dev/null
+  
   echo 'Done.'
   echo ''
 }
 
 ResolveCommands "$@"
-
+unset IFS
+# Reset (unset) IFS
